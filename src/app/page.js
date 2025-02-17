e124de8587d3e357385b3cbf77fe442efc89bdb6
Franzzuuu <114815424+Franzzuuu@users.noreply.github.com>
2025-02-17 15:43:49 +0800

    Added Background

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-green-900 text-white text-3xl font-bold">
-
-     
+    <div
+      className="flex min-h-screen items-center justify-center text-white text-3xl font-bold"
+      style={{
+        backgroundImage: 'url(/ismisbg.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       Welcome to ISMIS
-
-
     </div>
   );
 }
